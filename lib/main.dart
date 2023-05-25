7591ec6cbb0c826f0bf312038845637c5ca84b82
Heriberto <heribertobr.ti20@utsjr.edu.mx>
2023-05-24 21:21:07 -0600

    Actualización 24/05/2023

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:contador/pages/home_page.dart';
+//import 'package:contador/pages/home_page.dart';
+import 'package:contador/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 
@@ -10,8 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      //quitar la etiqueta de debugger
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: HomePage(), // en esta parte se manda llamar la pagina home_page
+      home: LoginPage(), // en esta parte se manda llamar la pagina home_page
     );
   }
 }
